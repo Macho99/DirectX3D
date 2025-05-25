13e389aae4497311d55d6b81d57dfaf6a7332fce
Macho99 <pes4825@naver.com>
2025-05-25 16:49:39 +0900

    feat : 쉐도우맵 카메라 앞쪽으로 그리게해서 보이는 범위 늘리기

--- Engine/Light.cpp
+++ Engine/Light.cpp
@@ -23,9 +23,10 @@ void Light::Update()
 void Light::SetVPMatrix(Camera* camera, float backDist, Matrix matProjection)
 {
 	Vec3 camPos = camera->GetTransform()->GetPosition();
+	Vec3 camLook = camera->GetTransform()->GetLook();
 	Vec3 lookVec = GetTransform()->GetLook();
 
-	Vec3 eyePosition = camPos - lookVec * backDist;
+	Vec3 eyePosition = camPos - lookVec * backDist + camLook * 50;
 	Vec3 focusPosition = eyePosition + lookVec;
 	Vec3 upDirection = Vec3::Up;
 	S_MatView = ::XMMatrixLookAtLH(eyePosition, focusPosition, upDirection);
