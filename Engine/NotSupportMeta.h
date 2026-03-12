99838e54f9685915dfee2d3dddc8a36a82b5c1b6
Macho99 <pes4825@naver.com>
2026-03-12 23:12:09 +0900

    feat : 미지원 메타 LoadResource 막기

--- Engine/NotSupportMeta.h
+++ Engine/NotSupportMeta.h
@@ -6,5 +6,11 @@ class NotSupportMeta : public MetaFile
 public:
     NotSupportMeta() : Super(ResourceType::None) {}
     ~NotSupportMeta() = default;
+
+    virtual unique_ptr<ResourceBase> LoadResource(AssetId assetId) const override
+    {
+        assert(false && "NotSupportMeta::LoadResource: This resource type is not supported.");
+        return nullptr;
+    }
 };
 
